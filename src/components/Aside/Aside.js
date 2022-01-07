b39137094d2c6bd1a4a9e7c33f2ede1bd2e7da70
iqbalmnaufal <iqbalmnaufal@gmail.com>
2022-01-07 16:15:30 +0700

    add menu in aside

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,7 +1,20 @@
+import dashboardIcon from "../../assets/Dashboard-icon.png";
+
 import classes from "./Aside.module.css";
 
 const Aside = () => {
-  return <aside></aside>;
+  return (
+    <aside>
+      <div className={classes.hamburgerContainer}>
+        <div className={classes.hamburger}></div>
+        <div className={classes.hamburger}></div>
+        <div className={classes.hamburger}></div>
+      </div>
+      <div className={classes.dashboardIconContainer}>
+        <img className={classes.dashboardIcon} src={dashboardIcon} alt="Dashboard Icon" />
+      </div>
+    </aside>
+  );
 };
 
 export default Aside;
